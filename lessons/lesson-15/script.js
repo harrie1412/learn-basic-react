18992d4c927471318da8c403cc20a415f545259e
Harri <harri.oceanz@gmail.com>
2025-05-19 00:10:44 +0700

    Day lan dau  lên repo

---
+++ lessons/lesson-15/script.js
@@ -0,0 +1,35 @@
+// function sum(a,b){
+//     alert(a+b);
+// }
+// sum(10,20);
+
+// var infoUser = {
+//     name: "Khánh Hà",
+//     hometown: "Khánh Hòa",
+//     age: function(a,b){
+//         alert(b-a);
+//     }
+// }
+// console.log(infoUser.name);
+// console.log(infoUser.hometown);
+// // infoUser.age(2005,2025);
+
+
+// var name= prompt("Bạn tên gì?","");
+// if(name === true){
+//     console.log("Đã nhập thành công!");
+// }
+// else {
+//     console.warn("Bạn chưa nhập!");
+// }
+
+// setInterval(function(){
+//     alert("Xin chào thế giới loài người!");
+// },200000000);
+
+var a = 10;
+var b = "Hello";
+var c;
+var d = true;
+// console.log(b.indexOf("Lo",1));
+console.log(b.toUpperCase());
